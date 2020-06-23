85c06494acb04b14a3b79ae4e663817585a770dc
quinn <quinn@quinnweber.com>
2020-06-22 21:21:08 -0700

    add upload route

--- src/geo_journal/app/http.py
+++ src/geo_journal/app/http.py
@@ -8,6 +8,8 @@ from raven import Client  # type: ignore
 from raven.contrib.flask import Sentry  # type: ignore
 from raven.transport.requests import RequestsHTTPTransport  # type: ignore
 
+from geo_journal.lib.exif import get_lat_long
+
 
 app = Flask(__name__)
 sentry = Sentry(app, client=Client(transport=RequestsHTTPTransport,),)
@@ -19,6 +21,27 @@ def status() -> Response:
     logger.info("recieved request with args {}".format(json.dumps(request.args)))
 
     response = jsonify({"text": "ok"})
+    response.headers.add("Access-Control-Allow-Origin", "*")
+
+    return typing.cast(Response, response)
+
+
+@app.route("/api/v0/upload", methods=["POST"])
+def upload() -> Response:
+    if "file" not in request.files:
+        raise Exception("file not uploaded")
+    file = request.files["file"]
+    if not file or file.filename == "":
+        raise Exception("file not uploaded")
+
+    path = "/tmp/foo"
+    file.save(path)
+
+    coordinates = get_lat_long(path)
+
+    response = jsonify(
+        {"text": {"latitude": coordinates.latitude, "longitude": coordinates.longitude}}
+    )
     response.headers.add("Access-Control-Allow-Origin", "*")
 
     return typing.cast(Response, response)
